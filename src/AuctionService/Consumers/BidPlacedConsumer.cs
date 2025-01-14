7c1685ff871c99011d18e48057fe82d96566e1f2
Junbin Deng <junbin.deng@gmail.com>
2025-01-14 13:02:14 +1300

    End of section 6

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,24 @@
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer(AuctionDbContext _dbContext) : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId)
+            ?? throw new MessageException(typeof(BidPlaced), "Cannot retrieve this auction");
+
+        if (auction.CurrentHighBid == null
+            || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
